a62c56cfaa17b51bcd49c071ff59190fa388d357
Lee Culver <leculver@microsoft.com>
2016-08-16 17:14:08 -0700

    Compress minidump files
    Compress minidump test data.

--- src/FileFormats.Minidump.Tests/Tests.cs
+++ src/FileFormats.Minidump.Tests/Tests.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
 using TestHelpers;
 using Xunit;
 
@@ -6,35 +10,65 @@ namespace FileFormats.Minidump
 {
     public class Tests
     {
-        [Fact(Skip ="Need data file")]
-        public void CheckLoadedModules()
-        {
-            using (Stream stream = File.OpenRead("TestBinaries/crash_x86.dmp"))
-            {
-                StreamAddressSpace sas = new StreamAddressSpace(stream);
-                Minidump minidump = new Minidump(sas);
+        const string x86Dump = "TestBinaries/minidump_x86.dmp.gz";
+        const string x64Dump = "TestBinaries/minidump_x64.dmp.gz";
 
-                var modules = minidump.LoadedImages;
-                Assert.True(modules.Length > 0);
-            }
+        static Tests()
+        {
+            
         }
 
-        [Fact(Skip = "Need data file")]
+        [Fact]
+        public void CheckLoadedModules()
+        {
+            using (Stream stream = GetCrashDump(x86Dump))
+                CheckLoadedModules(stream);
+
+            using (Stream stream = GetCrashDump(x64Dump))
+                CheckLoadedModules(stream);
+        }
+
+        private static void CheckLoadedModules(Stream stream)
+        {
+            IAddressSpace sas = new StreamAddressSpace(stream);
+            Minidump minidump = new Minidump(sas);
+
+            var modules = minidump.LoadedImages;
+            Assert.True(modules.Length > 0);
+        }
+
+        [Fact]
         public void CheckStartupMemoryRead()
         {
-            using (Stream stream = File.OpenRead("TestBinaries/crash_x86.dmp"))
-            {
-                StreamAddressSpace sas = new StreamAddressSpace(stream);
-                MaxStreamReadHelper readHelper = new MaxStreamReadHelper(sas);
+            using (Stream stream = GetCrashDump(x86Dump))
+                CheckStartupMemoryRead(stream);
 
-                Minidump minidump = new Minidump(readHelper);
+            using (Stream stream = GetCrashDump(x64Dump))
+                CheckStartupMemoryRead(stream);
+        }
 
-                // We should have read the header of a minidump, so we cannot have read nothing.
-                Assert.True(readHelper.Max > 0);
 
-                // We should only read the header and not too far into the dump file, 1k should be plenty.
-                Assert.True(readHelper.Max <= 1024);
-            }
+        private static void CheckStartupMemoryRead(Stream stream)
+        {
+            IAddressSpace sas = new StreamAddressSpace(stream);
+            MaxStreamReadHelper readHelper = new MaxStreamReadHelper(sas);
+
+            Minidump minidump = new Minidump(readHelper);
+
+            // We should have read the header of a minidump, so we cannot have read nothing.
+            Assert.True(readHelper.Max > 0);
+
+            // We should only read the header and not too far into the dump file, 1k should be plenty.
+            Assert.True(readHelper.Max <= 1024);
+        }
+
+        private Stream GetCrashDump(string path)
+        {
+            MemoryStream ms = new MemoryStream();
+            using (FileStream fs = File.OpenRead(path))
+            using (GZipStream gs = new GZipStream(fs, CompressionMode.Decompress))
+                gs.CopyTo(ms);
+            return ms;
         }
     }
 }
